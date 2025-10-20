b9dad6453e2fdb2e513f760468a36b7fdbc8bac7
akintun <69600964+akintun@users.noreply.github.com>
2025-10-20 16:03:05 -0700

    Create LoadingSpinner.tsx

---
+++ src/components/layout/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+export const LoadingSpinner = () => (
+  <div className="flex min-h-screen items-center justify-center bg-background">
+    <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+  </div>
+);
